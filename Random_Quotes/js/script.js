a2b22814f2f455893d5691caed702993fa909903
miguelfondeur <miguel@solveitsimply.com>
2016-05-14 23:53:05 -0400

    finish challenge

--- Random_Quotes/js/script.js
+++ Random_Quotes/js/script.js
@@ -1,61 +1,105 @@
-'use strict';
+//Miguel Fondeur, Lover of all things teamtreehouse.
+
 
 // event listener to respond to clicks on the page
 // when user clicks anywhere on the page, the "makeQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+var elem = document.getElementById('loadQuote');
+elem.addEventListener("click", function(){
+  printQuote();
+  // PrintQuote every 25 seconds
+  setInterval(printQuote, 25000);
+}, false);
 
-// Create an array of JavaScript objects to hold the data for your quotes.
+
+//Global counter variable
+var i = 0;
+
+
+//----------Array of Quote objects
 var quotes = [
   {
-    quote: 'Stay Hungry. Stay Foolish.',
+    quote: '<b>Stay Hungry <br> Stay Foolish</b>',
     source: 'Steve Jobs',
     citation: 'Stanford Commencement Speech',
-    year: '2005'
+    year: '2005',
+    font: ''
   },
   {
-    quote: 'The thing about perfection is that it\'s unknowable. It\'s impossible, but it\'s also right in front of us all the time.',
+    quote: 'The thing about perfection is that it\'s <b>unknowable</b> It\'s <b>impossible</b> but it\'s also <b>right in front of us</b> all the time.',
     source: 'Kevin Flynn',
     citation: 'Tron Legacy',
-    year: '2010'
+    year: '2010',
+    font: ''
   },
   {
-    quote: 'We accept the reality of the world with which we are presented, it\'s as simple as that.',
+    quote: '<b>We accept</b> the reality of <b>the world</b> with which <b>we are presented</b> it\'s as simple as that.',
     source: 'Christof',
     citation: 'The Truman Show',
-    year: '1998'
+    year: '1998',
+    font: ''
   },
   {
-    quote: 'We are the music makers, And we are the dreamers of dreams...',
+    quote: 'We are the <b>music makers,</b> And we are the <b>dreamers of dreams</b>',
     source: 'Arthur O\'Shaughnessy',
     citation: 'Ode',
-    year: '1874'
+    year: '1874',
+    font: ''
   },
   {
-    quote: 'For whatever was written in former days was written for our instruction, that through endurance and through the encouragement of the Scriptures we might have hope.',
-    source: 'Saul of Tarsus',
+    quote: 'For <b>whatever was written</b> in former days <b>was written for</b> our instruction, that through endurance and through the encouragement of the Scriptures we might have <b>hope</b>',
+    source: 'Paul',
     citation: 'Romans 15:4',
-    year: '58'
+    year: '58',
+    font: ''
   },
   {
-    quote: 'The light that burns twice as bright burns half as long and you have burned so very, very brightly',
+    quote: 'The light that burns <b>twice as bright</b> burns <b>half as long</b> and you have burned<br> so very, very brightly',
     source: 'ROY',
     citation: 'Blade Runner',
-    year: '1982'
+    year: '1982',
+    font: ''
+  },
+  {
+    quote: '<b> The true mind</b> can weather all the lies and illustions <br> without being lost. <b> The true heart</b> can touch the poison of hatred<br> without being harmed',
+    source: 'Great Lion Turtle',
+    citation: 'Avatar - The Last Airbender',
   }
 ];
 
-// Create a function named getRandomQuote which:
+
+//----------Get Random Quote
 function getRandomQuote() {
   //select a random quote object from the quotes array
   var i = Math.floor(Math.random() * quotes.length );
   //return the randomly selected quote object
   return quotes[i];
-};
+}
 
-// Create a function named printQuote
+//----------Create RGB Color
+function rgbColor() {
+  //Randomly creates rgb color values
+  var red   = Math.floor(Math.random() *  100),
+      green = Math.floor(Math.random() *  100),
+      blue  = Math.floor(Math.random() *  100);
+  //Creates Color from rgb values
+  var color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  //Returns Color
+  return color;
+}
+
+//----------Print Quote
 function printQuote() {
-  //calls the getRandomQuote function and stores the returned quote object in a variable
-  var quote = getRandomQuote();
+
+  //Store current quote object in quote variable
+  var quote = quotes[i];
+  //Increase counter to cycle through quotes
+  i ++;
+
+  //Get Random Quote if counter > quotes.length
+  if (i > quotes.length ) {
+    //call the getRandomQuote function and store the returned quote object in a variable
+    quote = getRandomQuote();
+  }
 
   // construct a string using the different properties of the quote object using the following HTML template
   var string = '';
@@ -64,12 +108,17 @@ function printQuote() {
   //Source
   string += '<p class="source">' + quote.source ;
   //Citation
-  string += '<span class="citation">' + quote.citation + '</span>';
+  if ( quote.citation ) {
+    string += '<span class="citation">' + quote.citation + '</span>';
+  }
   //Year
-  string += '<span class="year">' + quote.year + '</span>';
+  if ( quote.year ) {
+    string += '<span class="year">' + quote.year + '</span>';
+  }
   //Close Source
   string += '</p>';
 
-  //display the final HTML string to the page
+  //display the final HTML string to the page and print Random Color
+  document.body.style.backgroundColor = rgbColor();
   document.getElementById('quote-box').innerHTML = string;
-};
+}
